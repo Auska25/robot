4d0c4507b99bda83466f148ef788b397e6148ec3
Karl Austin <karl.austin@outlook.com>
2015-06-23 15:38:25 +1000

    DirectionSouth left() and right()

--- ruby/src/DirectionSouth.rb
+++ ruby/src/DirectionSouth.rb
@@ -1,8 +1,18 @@
 require_relative "IDirection.rb"
+require_relative "DirectionWest.rb"
+require_relative "DirectionEast.rb"
 
 class DirectionSouth < IDirection
   
   def name()
     return "SOUTH"
+  end
+  
+  def left()
+    return DirectionEast.new()
+  end
+  
+  def right()
+    return DirectionWest.new()
   end
 end
--- ruby/test/tc_DirectionSouth.rb
+++ ruby/test/tc_DirectionSouth.rb
@@ -8,4 +8,17 @@ class TestDirectionSouth < Test::Unit::TestCase
     assert_equal("SOUTH", south.name())
   end
   
+  def test_left
+    direction = DirectionSouth.new()
+    assert_equal("SOUTH", direction.name())
+    direction = direction.left()
+    assert_equal("EAST", direction.name())
+  end
+  
+  def test_right
+    direction = DirectionSouth.new()
+    assert_equal("SOUTH", direction.name())
+    direction = direction.right()
+    assert_equal("WEST", direction.name())
+  end
 end
